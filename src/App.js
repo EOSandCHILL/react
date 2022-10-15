94337a66b920df4680bccc74ca9330c84fb4ac20
MackInTech <jerrykmack803@yahoo.com>
2022-10-14 23:40:03 -0400

    learned about components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
+import Employee from "./components/employee";
 
 function App() {
+  console.log("we are about to list the employees");
+  const showEmployees = false;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployees ? (
+        <>
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+        </>
+      ) : (
+        <p>dont show the employees</p>
+      )}
     </div>
   );
 }
---
+++ src/components/employee.js
@@ -0,0 +1,8 @@
+function Employee() {
+  return <h3>Here is an emploee</h3>;
+}
+export default Employee;
+
+/*
+Components are encapsulate functionality and the logic. Theyre also able to have content or better known as html and they can have the design. This means our application is built in these self-sustained components and the different technologies are not artificially split out across different sections of the application making it easier to know what each component does and how it should look and act alone vs artificially split across the application causing confusion
+*/
