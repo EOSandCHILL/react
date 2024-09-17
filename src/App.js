0f9eaf450b851bb207dca771e27adc384e2c867a
MackInTech <jerrykmack803@yahoo.com>
2024-09-16 22:56:55 -0400

    Solve deployment App error with Employee file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import "./index.css";
-import Employee from "./components/Employee";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import AddEmployee from "./components/AddEmployee";
 import EditEmployee from "./components/EditEmployee";
+import Employee from "./components/Employee";
 import Header from "./components/Header";
+import "./index.css";
 function App() {
-  const [setRole /*function*/] = useState("Engineer");
+  const [, setRole] = useState("Engineer");
   const [employees, setEmployees] = useState([
     {
       id: 1,
