611b69577c96064fd60f2802202b5a73d726ed4a
MackInTech <jerrykmack803@yahoo.com>
2024-09-16 22:27:22 -0400

    Solve deployment App error with Employee file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import Employee from "./components/Employee.js";
+import Employee from "./components/Employee";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import AddEmployee from "./components/AddEmployee";
